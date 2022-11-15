5f17596e9076c030f043e84b12360264fd080265
Svetlana Petrunenkova <gtnheytyrjdf1@yandex.ru>
2022-11-15 19:04:36 +0300

    update recipesList, add filter on meals

--- components/RecipesPage.js
+++ components/RecipesPage.js
@@ -1,11 +1,17 @@
 app.component('recipes-page', {
     template: 
     /*html*/
-  `<h2 @click=showRecipes>Рецепты</h2>
+  `<h2>Рецепты</h2>
+    <div class="filter">
+        <div>
+            <button :class= "{currentFilter: selectedFilter === 'breakfast lunch dinner'}" @click="selectedFilter = 'breakfast lunch dinner'">Все</button>
+            <button v-for="meal in meals" :class= "{currentFilter: selectedFilter === meal.title}" @click="selectedFilter=meal.title">{{meal.name}}</button>
+        </div>
+    </div>
     <div class="recipes">
-      <div v-for="recipe in recipesList" class="recipe">
+      <div v-for="recipe in filteredRecipes" class="recipe">
       <h3>{{ recipe.title }} <span class="calories"> {{ recipe.calories }} ккал</span></h3>
-      <h6> {{ recipe.meal }}</h6>
+      <h5 class="recipeMeal"> <i class="fa-regular fa-clock"></i> {{ getMeal(recipe.meal) }}</h5>
       <div class="ingredients">
           <ul>
               <li v-for="ingredient in recipe.ingredients">{{ ingredient.name }} <span v-if="ingredient.quantity === 0" class="calories">{{ingredient.measure}}</span><span v-else class="calories">{{ingredient.quantity}} {{ingredient.measure}}</span></li>
@@ -19,11 +25,20 @@ app.component('recipes-page', {
     `,
     data() {
         return {
-            meals: ['Завтрак', 'Обед', 'Ужин'],
+            meals: [{title:'breakfast', name:'Завтрак'}, {title:'lunch', name:'Обед'}, {title:'dinner', name:'Ужин'}],
             recipesList: [...recipes],
+            selectedFilter: 'breakfast lunch dinner',
         }
     },
     methods: {
-        
+        getMeal(title) {
+            const currMeal = this.meals.find((meal) => meal.title === title);
+            return currMeal.name;
+        },
+    },
+    computed: {
+        filteredRecipes() {
+            return [...this.recipesList].filter((recipe) => this.selectedFilter.includes(recipe.meal))
+        },
     },
 });
